bdb6faa8238baad607cd62b1af828c8d121296e3
luolamies96 <tommijouppila@hotmail.com>
2022-11-24 09:56:57 +0200

    datanlukua

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Chart } from 'chart.js/auto';
+import { Line } from 'react-chartjs-2';
+import  maunaloaco2 from "./data/maunaloaco2.json";
 function App() {
+  const data = {
+    datasets: [
+      {
+        label: "emt",
+        data: [...maunaloaco2],
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+        yAxisID: "year",
+        parsing: {
+          xAxisKey: "year",
+          yAxisKey: "mean",
+        },
+        pointRadius: 1,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "maunaloaCO2levels",
+      },
+    },
+    scales: {
+      year: {
+        type: "linear",
+        display: true,
+        position: "right",
+      },
+    },
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ width: "1000px" }}>
+        <Line options={options} data={data} />
+      </div>
     </div>
   );
 }
+
 
 export default App;
